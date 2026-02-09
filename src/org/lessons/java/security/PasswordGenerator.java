1662889ca6dfdc24c5dfce2b385b729fc98d3a1a
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-09 13:34:47 +0100

    add firstname, secondnname, and favouritecolour

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,18 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write your first name");
+        String firstName = input.nextLine();
+        System.out.println("Write your second name");
+        String secondName = input.nextLine();
+        System.out.println("Write your favourite colour");
+        String favouriteColour = input.nextLine();
+        System.out.println(
+            String.format("")
+        );
     }
 }
