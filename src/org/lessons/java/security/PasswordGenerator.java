369e0c75914b5ed55e9b474bd911696311bf8161
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-09 13:39:59 +0100

    add comments

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
+        // Variabile che consente di dare valori alle variabili dal terminale
         Scanner input = new Scanner(System.in);
+
+        // Nome
         System.out.println("Write your first name");
         String firstName = input.nextLine();
+
+        // Cognome
         System.out.println("Write your second name");
         String secondName = input.nextLine();
+
+        // Colore preferito
         System.out.println("Write your favourite colour");
         String favouriteColour = input.nextLine();
+
+        // Output della password, metodo utilizzato per codice più pulito
         System.out.println(
             String.format("Your password is: %s-%s-%s",
                 firstName,
